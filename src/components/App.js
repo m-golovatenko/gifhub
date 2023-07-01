263c12742f6567a1375a404041018e785521cf32
Maria Golovatenko <pikch4y@yandex.com>
2023-07-01 18:23:39 +0300

    feat: создать отдельные классы под страницы

--- src/components/App.js
+++ src/components/App.js
@@ -4,39 +4,37 @@ import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
 import api from '../utils/api';
+import Trending from './Trending';
 
 function App() {
-  const [gifs, setGifs] = React.useState([]);
+  const [searchGifs, setSearchGifs] = React.useState([]);
+  const [trendingGifs, setTrendingGifs] = React.useState([]);
 
   React.useEffect(() => {
-    api.getTrendingGifs()
-        .then(data => {
-          setGifs(data.data);
-        })
-        .catch(error => {
-          console.error(error);
-        })
-  }, [])
-  
+    api
+      .getTrendingGifs()
+      .then(data => {
+        setTrendingGifs(data.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="page">
       <Header />
 
       <Routes>
-        <Route path="/search" element={
-          <Main isSearchActive={true} gifs={gifs} />
-        } />
+        <Route path="/search" element={<Main gifs={searchGifs} />} />
 
-        <Route path="/gifs" element={
-          <Main isSearchActive={false} gifs={gifs} />
-        } />
+        <Route path="/gifs" element={<Trending gifs={trendingGifs} />} />
 
         {/* <Route path="/random-gif" element={
           <RandomGif />
         } /> */}
 
-        <Route path="*" element={<Navigate to="/search" replace /> }/>
-
+        <Route path="*" element={<Navigate to="/search" replace />} />
       </Routes>
 
       {/* <Main /> */}
--- src/components/Trending.js
+++ src/components/Trending.js
@@ -2,25 +2,20 @@ import React from 'react';
 import Pagination from './Pagination';
 import Gif from './Gif';
 
-function Main({ gifs }) {
+function Trending({ gifs }) {
   console.log(gifs);
 
   return (
     <div className="container">
-      <form className="search">
-        <input className="search__bar" placeholder="Поле поиска" />
-        <button className="search__clear" />
-        <button className="search__submit" />
-      </form>
-
       <div className="gifs">
         {gifs.map(gif => (
           <Gif key={gif.id} gifUrl={gif.images.original.url} title={gif.title} />
         ))}
       </div>
+
       <Pagination />
     </div>
   );
 }
 
-export default Main;
+export default Trending;
