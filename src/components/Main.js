94b3c862f629ab59860fc472242f554ee2153e8e
Masha Golovatenko <pikch4y@yandex.com>
2023-07-01 11:48:18 +0300

    feat: сделать оверлей на картинке

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import gif from '../images/gifs__image.png';
 
 function Main() {
   return (
@@ -9,16 +10,48 @@ function Main() {
         <button className="search__submit" />
       </form>
       <div className="gifs">
-        <div className="gifs__item"></div>
-        <div className="gifs__item"></div>
-        <div className="gifs__item"></div>
-        <div className="gifs__item"></div>
-        <div className="gifs__item"></div>
-        <div className="gifs__item"></div>
-        <div className="gifs__item"></div>
-        <div className="gifs__item"></div>
-        <div className="gifs__item"></div>
+        <figure className="gifs__item">
+          <img className="gifs__image" src={gif} alt="" />
+          <figcaption className="gifs__title">123</figcaption>
+        </figure>
+
+        <figure className="gifs__item">
+          <img className="gifs__image" src={gif} alt="" />
+          <figcaption className="gifs__title">123</figcaption>
+        </figure>
+
+        <figure className="gifs__item">
+          <img className="gifs__image" src={gif} alt="" />
+          <figcaption className="gifs__title">123</figcaption>
+        </figure>
+
+        <figure className="gifs__item">
+          <img className="gifs__image" src={gif} alt="" />
+          <figcaption className="gifs__title">123</figcaption>
+        </figure>
+
+        <figure className="gifs__item">
+          <img className="gifs__image" src={gif} alt="" />
+          <figcaption className="gifs__title">123</figcaption>
+        </figure>
+        <figure className="gifs__item">
+          <img className="gifs__image" src={gif} alt="" />
+          <figcaption className="gifs__title">123</figcaption>
+        </figure>
+        <figure className="gifs__item">
+          <img className="gifs__image" src={gif} alt="" />
+          <figcaption className="gifs__title">123</figcaption>
+        </figure>
+        <figure className="gifs__item">
+          <img className="gifs__image" src={gif} alt="" />
+          <figcaption className="gifs__title">123</figcaption>
+        </figure>
+        <figure className="gifs__item">
+          <img className="gifs__image" src={gif} alt="" />
+          <figcaption className="gifs__title">123</figcaption>
+        </figure>
       </div>
+
       <div className="pages">
         <button className="pages__button">Назад</button>
         <ul className="pages__list">
