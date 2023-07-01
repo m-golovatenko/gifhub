b4259ff095cabf1fe229d5020591a8f0b0e444bc
Olga Bondarenko <bonnhelga86@yandex.ru>
2023-07-01 11:07:55 +0400

    feat: Вынести пагинацию в отдельный компонент Pagination, добавить Route

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Pagination from './Pagination';
 
-function Main() {
+function Main({ isSearchActive }) {
   return (
     <div className="container">
-      <form className="search">
+
+      { isSearchActive && <form className="search">
         <input className="search__bar" placeholder="Поле поиска" />
         <button className="search__clear" />
         <button className="search__submit" />
-      </form>
+      </form> }
+
       <div className="gifs">
         <div className="gifs__item"></div>
         <div className="gifs__item"></div>
@@ -19,18 +22,8 @@ function Main() {
         <div className="gifs__item"></div>
         <div className="gifs__item"></div>
       </div>
-      <div className="pages">
-        <a className="pages__item">Назад</a>
-        <a className="pages__item">1</a>
-        <a className="pages__item">2</a>
-        <a className="pages__item">3</a>
-        <a className="pages__item">4</a>
-        <a className="pages__item">5</a>
-        <a className="pages__item">6</a>
-        <a className="pages__item">7</a>
-        <a className="pages__item">8</a>
-        <a className="pages__item">Вперед</a>
-      </div>
+
+      <Pagination />
     </div>
   );
 }
