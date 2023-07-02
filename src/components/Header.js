c3289dffbbad58afaaed56c19b0d96b1a881bb2d
Maria Golovatenko <pikch4y@yandex.com>
2023-07-02 20:34:06 +0300

    style: добавить стили активных ссылок

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,41 @@
 import React from 'react';
-import { Link  } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
-  
-
   return (
     <header className="header">
       <a className="header__logo" href="#" aria-label="Логотип сайта." />
       <nav>
         <ul className="header__nav">
           <li>
-            <a href="/search" className="header__link">Поиск</a>
+            <NavLink
+              to="/search"
+              className={({ isActive }) =>
+                isActive ? ' header__link header__link_active' : 'header__link'
+              }
+            >
+              Поиск
+            </NavLink>
           </li>
           <li>
-            <a href="/trending/1" className="header__link">Тренды</a>
+            <NavLink
+              to="/trending/1"
+              className={({ isActive }) =>
+                isActive ? ' header__link header__link_active' : 'header__link'
+              }
+            >
+              Тренды
+            </NavLink>
           </li>
           <li>
-            <a href="/random-gif" className="header__link">Случайный гиф</a>
+            <NavLink
+              to="/random-gif"
+              className={({ isActive }) =>
+                isActive ? ' header__link header__link_active' : 'header__link'
+              }
+            >
+              Случайный гиф
+            </NavLink>
           </li>
         </ul>
       </nav>
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,25 +1,25 @@
-import { useParams, Link  } from 'react-router-dom';
+import { useParams, Link, NavLink } from 'react-router-dom';
 
 function Pagination({ pagination }) {
   let { page } = useParams();
-  const totalCountPage = Math.floor(pagination.total_count/pagination.count);
-  
+  const totalCountPage = Math.floor(pagination.total_count / pagination.count);
+
   function getPaginationList() {
     const paginationList = [];
 
     if (page < 4) {
-      for(let i = 1; i < 6; i++) {
+      for (let i = 1; i < 6; i++) {
         paginationList.push(i);
-        if(i === totalCountPage) {
+        if (i === totalCountPage) {
           break;
         }
       }
-    } else if(page > 3 && totalCountPage-page > 2) {
+    } else if (page > 3 && totalCountPage - page > 2) {
       for (let i = -2; i < 3; i++) {
-        paginationList.push(+page+i);
+        paginationList.push(+page + i);
       }
-    } else if(totalCountPage-page < 3 && totalCountPage-page >= 0) {
-      for (let i = totalCountPage-4; i <= totalCountPage; i++) {
+    } else if (totalCountPage - page < 3 && totalCountPage - page >= 0) {
+      for (let i = totalCountPage - 4; i <= totalCountPage; i++) {
         paginationList.push(i);
       }
     }
@@ -29,18 +29,32 @@ function Pagination({ pagination }) {
   return (
     <div className="pages">
       <ul className="pages__list">
-        {(page > 3) && <Link to="/trending/1" className="pages__button">В начало</Link>}
+        {page > 3 && (
+          <Link to="/trending/1" className="pages__nav">
+            В начало
+          </Link>
+        )}
 
         {getPaginationList().map((page, i) => {
           return (
-            <li key={i} className="pages__item">
-              <Link to={`/trending/${page}`}>{page}</Link>
+            <li key={i}>
+              <NavLink
+                to={`/trending/${page}`}
+                className={({ isActive }) =>
+                  isActive ? ' pages__item pages__item_active' : 'pages__item'
+                }
+              >
+                {page}
+              </NavLink>
             </li>
-          )
+          );
         })}
 
-        {(totalCountPage-page > 3) && <Link to={`/trending/${+page+1}`} className="pages__button">Вперед</Link>}
-
+        {totalCountPage - page > 3 && (
+          <Link to={`/trending/${+page + 1}`} className="pages__nav">
+            Вперед
+          </Link>
+        )}
       </ul>
     </div>
   );
